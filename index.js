207056cf4b32f1c42d71535c97ac88f2d97320f1
Tawhid Ali <tawhidali126@gmail.com>
2020-05-12 01:55:47 +0000

    depthFirstSearch Preorder created

--- index.js
+++ index.js
@@ -83,6 +83,22 @@ class BinarySearchTree {
   }
 
 
+  depthFirstSearchPreOrder(){
+    let data = []
+
+    function traverse(node){
+      data.push(node.value)
+      if(node.left){
+        traverse(node.left)
+      }
+      if(node.right){
+        traverse(node.right)
+      }
+    }
+
+    traverse(this.root)
+    return data
+  }
 
 
 }
