2be737f2d2cdca1d5d1aa6fe7fac632324d31353
Tawhid Ali <tawhidali126@gmail.com>
2020-05-12 00:36:12 +0000

    added breadthFirstSearch

--- index.js
+++ index.js
@@ -61,6 +61,30 @@ class BinarySearchTree {
   }
 
 
+  breadthFirstSearch(){
+    let node = this.root
+    let queue = []
+    let data = []
+    queue.push(node)
+
+    while(queue.length){
+      node = queue.shift()
+      data.push(node.value)
+
+      if(node.left){
+        queue.push(node.left)
+      }
+      if(node.right){
+        queue.push(node.right)
+      }
+    }
+
+    return data 
+  }
+
+
+
+
 }
 
 
